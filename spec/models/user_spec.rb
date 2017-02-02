b14266e35180eaaeb5b6d007af0593ce306c8a24
Leonardo Olivares <leonardo.olivares@wikot.com>
2017-02-02 13:57:28 -0300

    Se agrega prueba de validación de email en modelo

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "create user" do
+  it "crear usuario" do
     email = "test@tests.com"
     image = "123123"
     User.create(email: email, image:image)
 
     expect(User.count).to eq(1)
+  end
+
+  it "usuario con email valido" do
+    email = "email"
+    image = "123123"
+    begin
+        user = User.create!(email: email, image: image)
+    rescue
+        user = nil
+    end
+    expect(user).to eq(nil)
   end
 end
