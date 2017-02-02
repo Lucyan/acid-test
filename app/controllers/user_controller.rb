b567f5c1aece634aa5869e0c9035e5890689a09f
Leonardo Olivares <leonardo.olivares@wikot.com>
2017-02-02 15:05:12 -0300

    - Se agrega método login el cual realiza la llamada al webservice2 - Se agrega ruta login a scope rest - Se restringe llamada post a verify solo al servidor local

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,20 @@
 class UserController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
+  # Verifica email e imagen contra modelo
   def verify
     user = User.where(email: params[:email], image: params[:image]).first
     response = user.blank? ? ['No Autorizado', :unauthorized] : ['OK', :ok]
+    render :json => { :message => response[0] }, status: response[1]
+  end
+
+  # Obtiene los parametros de email e imagen y realiza llamada al servicio verify mediante la gema HTTParty
+  def login
+    base_url = 'http://localhost:3000/rest'
+    api_response = HTTParty.post("#{base_url}/verify_user/#{params[:email]}",
+                      :body => { 'image' => params[:image] }.to_json,
+                      :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
+    response = api_response.code == 401 ? ['No Autorizado', :unauthorized] : ['OK', :ok]
     render :json => { :message => response[0] }, status: response[1]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
 
   scope 'rest' do
-    post 'verify_user/:email', to: 'user#verify', constraints: { email: /[^\/]+/ }
+    post 'verify_user/:email', to: 'user#verify', constraints: { email: /[^\/]+/, ip: /127.0.0.1/ } #Se restringe solo a llamadas del mismo servidor local
+    post 'login', to: 'user#login'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
