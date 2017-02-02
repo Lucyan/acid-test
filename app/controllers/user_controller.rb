c084629ddc31e9ec0f0bfe8c945cb11e47157f42
Leonardo Olivares <leonardo.olivares@wikot.com>
2017-02-02 20:34:16 -0300

    - Se agrega validación de parámetros - Se utiliza ur_for

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,18 +3,23 @@ class UserController < ApplicationController
 
   # Verifica email e imagen contra modelo
   def verify
-    user = User.where(email: params[:email], image: params[:image]).first
+    user = nil
+    if params[:email] && params[:image]
+      user = User.where(email: params[:email], image: params[:image]).first
+    end
     response = user.blank? ? ['No Autorizado', :unauthorized] : ['OK', :ok]
     render :json => { :message => response[0] }, status: response[1]
   end
 
   # Obtiene los parametros de email e imagen y realiza llamada al servicio verify mediante la gema HTTParty
   def login
-    base_url = 'http://localhost:3000/rest'
-    api_response = HTTParty.post("#{base_url}/verify_user/#{params[:email]}",
+    response = ['No Autorizado', :unauthorized]
+    if params[:email] && params[:image] && params[:user_agent]
+      api_response = HTTParty.post(url_for(action: :verify, email: params[:email]),
                       :body => { 'image' => params[:image] }.to_json,
                       :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
-    response = api_response.code == 401 ? ['No Autorizado', :unauthorized] : ['OK', :ok]
+      response = ['OK', :ok] if api_response.code == 200
+    end
     render :json => { :message => response[0] }, status: response[1]
   end
 end
