0d73b7a5c468b36dbc3b1979aa8432789057d690
Leonardo Olivares <leonardo.olivares@wikot.com>
2017-02-03 02:51:06 -0300

    Se restringe llamada a verify vía token en header Authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
 
   # Verifica email e imagen contra modelo
   def verify
+    auth = request.headers["Authorization"] == 'aeqGNC313Xqe1P4uI765kAJqRPr13OTY' ? true : false # Solo se aceptan llamadas con token Authorization
     user = nil
-    if params[:email] && params[:image]
+    if params[:email] && params[:image] && auth
       user = User.where(email: params[:email], image: params[:image]).first
     end
     response = user.blank? ? ['No Autorizado', :unauthorized] : ['OK', :ok]
@@ -13,22 +14,13 @@ class UsersController < ApplicationController
 
   # Obtiene los parametros de email e imagen y realiza llamada al servicio verify mediante la gema HTTParty
   def login
-    local_ip = UDPSocket.open {|s| s.connect("64.233.187.99", 1); s.addr.last}
-    puts "*"*100
-    puts local_ip
-    puts request.remote_ip
-    puts "*"*100
-    auth = true
-    if Rails.env.production? && local_ip != request.remote_ip
-      auth = false
-    end
-
-
     response = ['No Autorizado', :unauthorized]
-    if params[:email] && params[:image] && params[:user_agent] && auth
+    if params[:email] && params[:image] && params[:user_agent]
       api_response = HTTParty.post(url_for(action: :verify, email: params[:email]),
-                      :body => { 'image' => params[:image] }.to_json,
-                      :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
+                      body: { 'image': params[:image] }.to_json,
+                      headers: { 'Content-Type': 'application/json', 
+                                    'Accept': 'application/json',
+                                    'Authorization': 'aeqGNC313Xqe1P4uI765kAJqRPr13OTY' }) # Token de autorización al llamar a verify
       response = ['OK', :ok] if api_response.code == 200
       EmailNotifierJob.perform_later(params[:email], response[0], params[:user_agent])
     end
