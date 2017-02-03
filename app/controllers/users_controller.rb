ac5cfee5c9cb6f87cb2eabac9a507aec9b1db458
Leonardo Olivares <leonardo.olivares@wikot.com>
2017-02-03 02:35:06 -0300

    Se agrega restricción en producción a endpoin verify_user para ser solo llamado por la misma ip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,19 @@ class UsersController < ApplicationController
 
   # Obtiene los parametros de email e imagen y realiza llamada al servicio verify mediante la gema HTTParty
   def login
+    local_ip = UDPSocket.open {|s| s.connect("64.233.187.99", 1); s.addr.last}
+    puts "*"*100
+    puts local_ip
+    puts request.remote_ip
+    puts "*"*100
+    auth = true
+    if Rails.env.production? && local_ip != request.remote_ip
+      auth = false
+    end
+
+
     response = ['No Autorizado', :unauthorized]
-    if params[:email] && params[:image] && params[:user_agent]
+    if params[:email] && params[:image] && params[:user_agent] && auth
       api_response = HTTParty.post(url_for(action: :verify, email: params[:email]),
                       :body => { 'image' => params[:image] }.to_json,
                       :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
