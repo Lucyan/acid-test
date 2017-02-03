f6eae3baf2e22beed2e23a50f06847810ef06b16
Leonardo Olivares <leonardo.olivares@wikot.com>
2017-02-03 12:34:12 -0300

    Corrección prueba automatizada

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe UsersController, type: :controller do
       image = "123123"
       User.create(email: email, image: image)
 
+      request.headers["Authorization"] = "aeqGNC313Xqe1P4uI765kAJqRPr13OTY"
       post :verify, params: {email: email, image: image}
       expect(response).to have_http_status(:ok)
     end
@@ -16,6 +17,7 @@ RSpec.describe UsersController, type: :controller do
       image = "123123"
       User.create(email: email, image: "321321")
       
+      request.headers["Authorization"] = "aeqGNC313Xqe1P4uI765kAJqRPr13OTY"
       post :verify, params: {email: email, image: image}
       expect(response).to have_http_status(:unauthorized)
     end
